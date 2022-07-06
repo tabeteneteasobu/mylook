5b87e79fc54ae9acfb74ffac8d1f949f7eec01c6
tabeteneteasobu <keeei.yamada@gmail.com>
2022-07-06 22:25:28 +0900

    ビューファイルの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,27 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id, :color_id, :brand, :size, :memo, :image)
   end
 end
+
+
