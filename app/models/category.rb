de3174b37c1eef9e9261275339cf628fd36807eb
tabeteneteasobu <keeei.yamada@gmail.com>
2022-07-08 17:08:52 +0900

    bottoms詳細ページの実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,10 @@ class Category < ActiveHash::Base
     { id: 1, name: '---' },
     { id: 2, name: 'トップス' },
     { id: 3, name: 'ボトムス' },
-    { id: 4, name: 'その他' },
+    { id: 4, name: 'ワンピース' },
+    { id: 5, name: '上着類' },
+    { id: 6, name: '小物類' },
+    { id: 7, name: 'その他' },
   ]
 
   include ActiveHash::Associations
