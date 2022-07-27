c4743a6ed4aa0d2e00cceda40b1d38ed75511b84
tabeteneteasobu <keeei.yamada@gmail.com>
2022-07-27 19:19:37 +0900

    itemアイコンの実装

--- app/javascript/item_menu.js
+++ app/javascript/item_menu.js
@@ -1,12 +1,14 @@
 window.addEventListener('load', function(){
-  console.log('menu.jsが読み込まれています');
-  const menu = document.getElementById("menu1");
+  console.log('item_menu.jsが読み込まれています');
+  const menu = document.getElementById("item_icon");
   const pullDownMenu = document.getElementById("menu1-pull-down");
 
   menu.addEventListener('click', function() {
+    console.log('プルダウンが読み込まれています');
     if (pullDownMenu.getAttribute("style") == "display:block;") {
       pullDownMenu.removeAttribute("style", "display:block;");
-    } else {
+    } 
+    else {
       pullDownMenu.setAttribute("style", "display:block;");
     }
   });
